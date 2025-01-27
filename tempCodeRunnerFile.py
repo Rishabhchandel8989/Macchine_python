974c833671e9123413a0d105ccc2225e8a2c124f
Rishabh Singh <rishabhsingh8989r@gmail.com>
2025-01-27 22:44:52 +0530

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -31,6 +31,20 @@ def get_db_connection():
         print(f"Database connection error: {err}")
         return None
 
+# for render
+def get_db_connection():
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME')
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Database connection error: {err}")
+        return None
+
 # Home route
 @app.route('/')
 def home():
